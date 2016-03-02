c4bd9be0ef24b9b21fdebc0f28ea1a431e11509b
kitianFresh <1549722424@qq.com>
2016-03-02 12:26:47 +0800

    实现shell排序

--- Sort/InsertSort.c
+++ Sort/InsertSort.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
+
+void PrintArray(int a[], int n);
+
 void InsertSort(int a[], int n);
 void BinSort(int a[], int n);
 
+void ShellInsert(int a[], int n, int d);
+void ShellSort(int a[], int n, int d[], int t);
+
 int main(){
 	int a[10];
+	int delta[3] = {5, 3, 1};
 	int i;
 	for (i = 0; i < 10; i++)
 		scanf("%d",a+i);
 	//InsertSort(a, 10);
-	BinSort(a, 10);
-	for (i = 0; i < 10; i++)
-		printf("%d ",a[i]);
-
+	//BinSort(a, 10);
+	ShellSort(a, 10, delta, 3);
+	PrintArray(a, 10);
 	return 0;
+}
+
+void PrintArray(int a[], int n){
+	int i;
+	for (i=0; i < n; i++)
+		printf("%d ",a[i]);
+	printf("\n");
 }
 
 void InsertSort(int a[], int n){
@@ -51,4 +64,25 @@ void BinSort(int a[], int n){
 		}
 		a[j+1] = temp;
 	}
+}
+
+void ShellInsert(int a[], int n, int d){
+	int trip, k, i, j, temp;
+	trip = n / d;
+	for (i = 0; i < trip; i++){
+		for (k = i + d; k < n; k+=d){
+			temp = a[k]; j = k - d;
+			while (j >= i && a[j] > temp) {
+				a[j+d] = a[j];
+				j -= d;
+			}
+			a[j+d] = temp;
+		}
+	}
+}
+
+void ShellSort(int a[], int n, int d[], int t){
+	int i = 0;
+	for (;i < t; i ++)
+		ShellInsert(a,n,d[i]);
 }
