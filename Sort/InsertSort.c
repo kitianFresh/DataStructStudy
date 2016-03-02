41638dffe6e971c4eaa502b4bedfaad85028cf7a
kitianFresh <1549722424@qq.com>
2016-03-02 10:25:35 +0800

    实现折半插入排序

--- Sort/InsertSort.c
+++ Sort/InsertSort.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 void InsertSort(int a[], int n);
+void BinSort(int a[], int n);
 
 int main(){
 	int a[10];
 	int i;
 	for (i = 0; i < 10; i++)
 		scanf("%d",a+i);
-	InsertSort(a,10);
+	//InsertSort(a, 10);
+	BinSort(a, 10);
 	for (i = 0; i < 10; i++)
 		printf("%d ",a[i]);
 
@@ -15,12 +17,37 @@ int main(){
 
 void InsertSort(int a[], int n){
 	int k,j,temp;
-	for (k = 1; k < n; k++){
+	for (k = 1; k < n; k++) {
 		temp = a[k];
 		j = k - 1;
 		while (j >= 0 && a[j] > temp) {
 			a[j+1] = a[j];
 			j--;
+		}
+		a[j+1] = temp;
+	}
+}
+
+void BinSort(int a[], int n){
+	int k,j,temp; /* For Insert */
+	int low,high,mid; /* For Binary Search */
+	for (k = 1; k < n; k++) {
+		temp = a[k];
+		
+		/* Search the position for inserting */
+		low = 0; high = k - 1;
+		while (low <= high) {
+			mid = (low + high)/2;
+			if (temp >= a[mid]) 
+				low = mid + 1;
+			else 
+				high = mid -1;
+		}
+		/* Move the position */
+		j = k -1;
+		while (j >= low) {
+			a[j+1] = a[j];
+			j --;
 		}
 		a[j+1] = temp;
 	}
