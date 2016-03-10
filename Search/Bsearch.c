4a5ff08f1c52c902c8c33c2c82889613dc1f406c
kitianFresh <1549722424@qq.com>
2016-03-10 15:24:02 +0800

    二分查找递归版本

--- Search/Bsearch.c
+++ Search/Bsearch.c
@@ -7,7 +7,7 @@ int main(int argc, int **agrv) {
 	ScanArray(a, 10);
 	PrintArray(a, 10);
 	scanf("%d", &key);
-	printf("index of %d is %d\n", key, Bsearch(a, 0, 9, key));
+	printf("index of %d is %d\n", key, Bsearch_r(a, 0, 9, key));
 	return 0;
 }
 
@@ -20,4 +20,16 @@ int Bsearch(int a[], int left, int right, int key) {
 		else right = mid - 1;
 	}
 	return -1;
+}
+
+int Bsearch_r(int a[], int left, int right, int key) {
+	int mid;
+	if (left > right) return -1;
+	mid = (left + right)/2;
+	if (a[mid] == key) 
+		return mid;
+	else if (a[mid] < key) 
+		return Bsearch_r(a, mid + 1, right, key);
+	else 
+		return Bsearch_r(a, left, mid - 1, key);
 }
